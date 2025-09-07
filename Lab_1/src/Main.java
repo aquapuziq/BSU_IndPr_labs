37422941dc967a3e209ad7c59af281e03f6172c6
Kirill Suhanov <kirillikirill86@gmail.com>
2025-09-07 16:00:56 +0300

    added a class for calculating a function using the Taylor series. implemented calculation of the factorial and raising to a power

--- Lab_1/src/Main.java
+++ Lab_1/src/Main.java
@@ -1,6 +1,24 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+class TaylorSeries {
+
+    private static double Calc_eps(int k) {
+        double eps = 1;
+        for (int i = 0; i < k; i++) {
+            eps /= 10;
+        }
+        return eps;
+    }
+
+    private static double Calc_fac(int n){
+        double fac_x = 1;
+        for (int i = 2; i <= n; i++) {
+            fac_x *= i;
+        }
+        return fac_x;
+    }
+}
 public class Main {
 
     public static void main(String[] args) {
