71094a426e9998b946d0b90e4ac72c675bfebde6
Kirill Suhanov <kirillikirill86@gmail.com>
2025-09-07 18:53:00 +0300

    clean code pt. 1

--- Lab_1/src/Main.java
+++ Lab_1/src/Main.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 
 class TaylorSeries {
 
-    private static double Calc_eps(int k) {
-        double eps = 1;
+    private static double calculationEpsilon(int k) {
+        double epsilon = 1;
         for (int i = 0; i < k; i++) {
-            eps /= 10;
+            epsilon /= 10;
         }
-        return eps;
+        return epsilon;
     }
 
-    private static double Calc_pow(double x, int n) {
+    private static double myPow(double x, int n) {
         double pow_x = 1;
         for (int i = 0; i < n; i++) {
             pow_x *= x;
@@ -19,31 +19,31 @@ class TaylorSeries {
         return pow_x;
     }
 
-    private static long Calc_fac(int n){
-        long fac_x = 1;
+    private static long calculationFactorial(int n){
+        long factorial_x = 1;
         for (int i = 2; i <= n; i++) {
-            fac_x *= i;
+            factorial_x *= i;
         }
-        return fac_x;
+        return factorial_x;
     }
 
-    private static double Calc_abs(double x) {
+    private static double myAbs(double x) {
         if (x < 0){
             x *= (-1);
         }
         return x;
     }
 
-    public static double Calc_sinh(double x, int k) {
+    public static double mySinh(double x, int k) {
         double shx = 0;
-        double eps = Calc_eps(k);
+        double epsilon = calculationEpsilon(k);
         for (int i = 0; ; i++){
             int n = i * 2 + 1;
-            long fac_n = Calc_fac(n);
-            double term = Calc_pow(x, n) / fac_n;
+            long factorial_n = calculationFactorial(n);
+            double term = myPow(x, n) / factorial_n;
 
-            double term_abs = Calc_abs(term);
-            if (term_abs < eps){
+            double term_abs = myAbs(term);
+            if (term_abs < epsilon){
                 break;
             } else {
                 shx += term;
@@ -52,6 +52,7 @@ class TaylorSeries {
         return shx;
     }
 }
+
 public class Main {
 
     public static void main(String[] args) {
@@ -75,10 +76,12 @@ public class Main {
                 System.out.println("Ошибка: число k должно быть натуральным");
             }
         }
+
         TaylorSeries sinhx = new TaylorSeries();
-        double mySinhx = sinhx.Calc_sinh(x, k);
+        double mySinhx = sinhx.mySinh(x, k);
         String res_1 = String.format("%.3f", mySinhx);
         System.out.println("Значение sinhx вычисленное приближенно: " + res_1);
+
         double stSinhx = Math.sinh(x);
         String res_2 = String.format("%.3f", stSinhx);
         System.out.print("Значение sinhx вычисленное через стандартную функцию Java: " + res_2);
