f44d29e0274dfaede55d5a4f4f007bcfcf15cfad
Kirill Suhanov <kirillikirill86@gmail.com>
2025-09-14 20:07:39 +0300

    list with all diagonally sums

--- Lab_2/src/Main.java
+++ Lab_2/src/Main.java
@@ -2,19 +2,20 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.Math;
 
 public class Main {
 
-    public static int calculateUpperDiagonallySum(int[][] matrix){
+    public static int calculateDiagonallySum(int[][] matrix){
         int sum = 0;
         int dim = matrix.length;
         for (int i = 0; i < dim; i++) {
-            sum += matrix[i][dim - 1 - i];
+            sum += Math.abs(matrix[i][dim - 1 - i]);
         }
         return sum;
     }
 
-    public static int[][] resizeMatrix(int[][] matrix, int newDim){
+    public static int[][] resizeUpMatrix(int[][] matrix, int newDim){
         int newMatrix[][] = new int[newDim][newDim];
         for(int i = 0; i < newDim; ++i) {
             for(int j = 0; j < newDim; ++j) {
@@ -24,11 +25,28 @@ public class Main {
         return newMatrix;
     }
 
-    public static List<Integer> upperSums (List<Integer> sums, int[][] matrix){
+    public static int[][] resizeDownMatrix(int[][] matrix, int newDim){
+        int newMatrix[][] = new int[newDim][newDim];
+        for(int i = 0; i < newDim; ++i) {
+            for(int j = 0; j < newDim; ++j) {
+                newMatrix[i][j] = matrix[newDim - i][newDim - j];
+            }
+        }
+        return newMatrix;
+    }
+
+    public static List<Integer> Sums (List<Integer> sums, int[][] matrix){
         int dim = matrix.length;
         for(int i = 1; i < dim; i++){
-            int[][] tmpMatrix = resizeMatrix(matrix, dim - i);
-            int tmpSum = calculateUpperDiagonallySum(tmpMatrix);
+            int[][] tmpUpMatrix = resizeUpMatrix(matrix, dim - i);
+            int tmpSum = calculateDiagonallySum(tmpUpMatrix);
+            sums.add(tmpSum);
+        }
+
+        int[][] tmpMatrix = resizeDownMatrix(matrix, dim - 1);
+        for(int i = 1; i < dim; i++){
+            int tmpDownMatrix[][] = resizeUpMatrix(tmpMatrix, dim - i);
+            int tmpSum = calculateDiagonallySum(tmpDownMatrix);
             sums.add(tmpSum);
         }
         return sums;
@@ -53,7 +71,8 @@ public class Main {
             System.out.print("\n");
         }
         List<Integer> elementsSums = new ArrayList<>();
-        elementsSums = upperSums(elementsSums, matrix);
-        System.out.println(elementsSums);
+        elementsSums = Sums(elementsSums, matrix);
+        System.out.println("Суммы модулей элементов диагоналей," +
+                " параллельных побочной диагонали: " + elementsSums);
     }
 }
