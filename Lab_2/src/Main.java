3379358ca12218d3c8666558ded180485da2b547
Kirill Suhanov <kirillikirill86@gmail.com>
2025-09-14 17:53:20 +0300

    fixed: matrix must be square

--- Lab_2/src/Main.java
+++ Lab_2/src/Main.java
@@ -5,21 +5,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
-        int topNum = 100;
-        System.out.print("Введите число столбцов матрицы: ");
-        int n = in.nextInt();
-        System.out.print("Введите число строк матрицы: ");
-        int m = in.nextInt();
-        int[][] matrix = new int[m][n];
 
-        for(int i = 0; i < m; ++i) {
-            for(int j = 0; j < n; ++j) {
+        int topNum = 100;
+
+        System.out.print("Введите размерность матрицы: ");
+        int dim = in.nextInt();
+
+        int[][] matrix = new int[dim][dim];
+
+        for(int i = 0; i < dim; ++i) {
+            for(int j = 0; j < dim; ++j) {
                 matrix[i][j] = rand.nextInt(topNum);
                 System.out.print(matrix[i][j] + " ");
             }
-
             System.out.print("\n");
         }
-
     }
 }
